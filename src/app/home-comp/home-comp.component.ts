56f29105819f6780a2e82957dc9251ebfead4bcb
harshanl <harshan.l@outlook.com>
2023-08-28 16:03:43 +0530

    right side view search finished

--- src/app/home-comp/home-comp.component.ts
+++ src/app/home-comp/home-comp.component.ts
@@ -1,5 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { getLocaleDayNames } from '@angular/common';
+// import * as CityNames from '../../assets/city.list.json'
 
 @Component({
   selector: 'app-home-comp',
@@ -11,7 +13,20 @@ export class HomeCompComponent implements OnInit{
   lan:any;
   climate:any;
   title="Weather app";
+  localname!:String;
   todate= new Date();
+  city:CityNames[]=[
+    new CityNames(1,"London"),
+    new CityNames(2,"Chennai"),
+    new CityNames(3,"New York"),
+  ];
+  num=4;
+  text:String="";
+  place="Search by City";
+  textera()
+  {
+    this.text="";
+  }
   url=`https://openweathermap.org/img/wn/`;
   constructor(private http:HttpClient)
   {
@@ -22,6 +37,8 @@ export class HomeCompComponent implements OnInit{
   }
   ngOnInit(): void {
     this.getlocation();
+    
+    // console.log(this.city);
   };
   getlocation()
   {
@@ -34,8 +51,70 @@ export class HomeCompComponent implements OnInit{
       console.log("position",this.lat,this.lan);
       this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lan}&units=metric&appid=d63ffdc782c304f7d34d21c57518639c`).subscribe((data)=>{
         this.climate=data;
+        this.localname=this.climate.name;
+        console.log(this.localname)
+        !this.isinclude(this.localname)?this.city.push(new CityNames(this.num++,this.localname)):console.log("not added");
         console.log(this.climate)
-      });   
+      });
+      // console.log(this.isinclude(this.climate.name)?"not added"+this.city.push(new CityNames(this.num++,this.climate.name)):this.city.push(new CityNames(this.num++,this.climate.name)),"added");
+      
   };
+
+  onsubmit()
+  {
+    
+    if(this.text!="")
+    {
+      this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.text}&units=metric&appid=d63ffdc782c304f7d34d21c57518639c`).subscribe((data)=>{
+        this.climate=data;
+        console.log(data);
+      },()=>{
+        alert("something went wrong");
+      });
+      console.log("submited",this.text)
+      !this.isinclude(this.text)?this.city.push(new CityNames(this.num++,this.text)):"not added";
+      this.text="";
+      console.log(this.city)
+    }
+    else{
+      console.log("something went wrong")
+    }
+    
+  }
+  isinclude(t:String)
+  {
+    for(let c of this.city)
+    {
+      if(c.name===t)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  getdate(num:number)
+  {
+    return new Date(num*1000).toUTCString();
+  }
+  geticon()
+  {
+    // console.log(this.localname)
+    return (this.climate.weather[0].icon as String).replaceAll('n','d');
+  }
+  display(n:number)
+  {
+    this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city[n-1].name}&units=metric&appid=d63ffdc782c304f7d34d21c57518639c`).subscribe((data)=>{
+        this.climate=data;
+        console.log(data);
+      },()=>{
+        alert("something went wrong");
+      });
+  }
   
+}
+class CityNames{
+  constructor(public id:number,public name:String)
+  {
+
+  }
 }
